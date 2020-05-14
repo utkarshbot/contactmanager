0f98ebbbf97a5438a7dda7fb2c10e1cb178a3a81
utkarshbot <you@example.com>
2020-05-14 13:47:56 +0530

    Updates

---
+++ precache-manifest.09a8876c4e6d9f4fad936c5b7c8d08e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b96b1dd2f647c53412e77fb6eb299ea",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3dcfb4d71fa095ae1574",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3dcfb4d71fa095ae1574",
+    "url": "/contactmanager/static/js/2.194c5291.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.194c5291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b2c8a72889e3ddecf4",
+    "url": "/contactmanager/static/js/main.0d1d33fc.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
